e65a1c6a7a04dd9ea01772fafafc2a5ff37fe103
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2024-01-20 16:44:19 +0000

    dataType done

---
+++ Basic_JS/dataType.js
@@ -0,0 +1,12 @@
+// number = 2^53
+//string =""
+// boolean = true,false 
+// null = standalone value 
+// bigInt
+// undefined = value is not assigned
+// symbol =  unique 
+
+let name = "Aditi"
+let age = 12
+
+console.log(typeof undefined);
